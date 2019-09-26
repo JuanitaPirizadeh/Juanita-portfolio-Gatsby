d298a370565f06a74f15b0c6124836a57fb5d705
JuanitaPirizadeh <jupirizadeh@gmail.com>
2019-09-26 12:11:21 +0200

    contentful

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,14 +2,19 @@ import React from 'react'
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import {graphql} from 'gatsby'
-
+import Banner from '../components/Banner'
 
 const blog = ({data}) => {
 
     return (
         <Layout>
-            <StyledHero img={data.blog6.childImageSharp.fluid}/>
+            <StyledHero img={data.blog6.childImageSharp.fluid}>
+            <Banner
+            title="The blog"
             
+            >
+            </Banner>
+           </StyledHero>
    </Layout>
     )
 }
